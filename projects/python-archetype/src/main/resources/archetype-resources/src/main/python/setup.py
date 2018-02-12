62848527f658a17b723910a59d40c75842a0e319
Dmitry Myasnikov <dmyasnikov@griddynamics.com>
2018-02-12 19:55:33 +0300

    Add minimal setup.py content to archetype

---
+++ projects/python-archetype/src/main/resources/archetype-resources/src/main/python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Archetype package',
+      version='0.1',
+      description='Archetype package for GD TechTalk',
+      url='https://github.com/savermyas/maven-pervertions',
+      author='Dmitry Myasnikov',
+      author_email='dmyasnikov@griddynamics.com',
+      license='GPLv3',
+      packages=['package'],
+      zip_safe=False)
